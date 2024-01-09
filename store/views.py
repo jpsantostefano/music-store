b86492e806748159dc1b87d791bd4af1ebf548a3
jpsantostefano <jpsantostefano@gmail.com>
2024-01-09 14:05:24 +0000

    category.html created with view. Navbar category options added

--- store/views.py
+++ store/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm
 from django import forms
+
+def category(request, cat):
+    try:
+        category = Category.objects.get(name=cat)
+        products = Product.objects.filter(category=category)
+        return render(request,'category.html',{'products':products,'category':category})
+    except:
+        messages.success(request, ("That category doesn't exist"))
+        return redirect('home')
 
 def product(request,pk):
     product = Product.objects.get(id=pk)
