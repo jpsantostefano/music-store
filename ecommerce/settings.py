49bed4b33c6b4fb4ace2d82535ba0a1c46ffdd2b
jpsantostefano <jpsantostefano@gmail.com>
2024-01-09 14:21:33 +0000

    cart app created. Linked with cart button on navbar

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'store',
+    'cart',
 ]
 
 MIDDLEWARE = [
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     path('accounts/', include('allauth.urls')),
+    path('cart/', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
