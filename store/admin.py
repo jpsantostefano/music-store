3e3574c0482d12bc8aefb85e84dc50ddea9eba12
jpsantostefano <jpsantostefano@gmail.com>
2024-01-13 21:26:28 +0000

    profile_view design done

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
-from .models import Category, Profile, Product, Order
+from django.contrib.auth.models import User, Group
+from .models import Profile
 
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(Profile)
+#Unregister Groups
+admin.site.unregister(Group)
+
+# Mix Profile info into User info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+#Extend User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    #Just display username fields on admin page
+    fields = ['username']
+    inlines = [ProfileInline]
+
+# Unregister initial User
+admin.site.unregister(User)
+
+# Registerer User and Profile
+admin.site.register(User, UserAdmin)
