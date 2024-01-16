3fbaed82099903fbd89f887afc4ac712bbba393e
jpsantostefano <jpsantostefano@gmail.com>
2024-01-16 21:12:16 +0000

    quantity buttons working on product_detail

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -74,7 +74,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'cart','templates',),
             os.path.join(BASE_DIR, 'templates', 'allauth')
             ],
         'APP_DIRS': True,
@@ -170,9 +170,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = ['static/']
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
 
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,112 +1,70 @@
+function handleEnableDisable(itemId) {
+    var currentValue = parseInt($(`.id_qty_${itemId}`).val());
+    var minusDisabled = currentValue < 2;
+    var plusDisabled = currentValue > 98;
 
-var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
-var clientSecret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripePublicKey);
-var elements = stripe.elements();
-var style = {
-    base: {
-        color: '#000',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-        fontSmoothing: 'antialiased',
-        fontSize: '16px',
-        '::placeholder': {
-            color: '#aab7c4'
+    $(`.decrement-qty_${itemId}`).prop('disabled', minusDisabled);
+    $(`.increment-qty_${itemId}`).prop('disabled', plusDisabled);
+}
+
+function updateQuantity(itemId, newQuantity) {
+    $.ajax({
+        type: 'POST',
+        url: '/update_quantity/',  // Reemplaza con la URL correcta en tu aplicación
+        data: {
+            item_id: itemId,
+            quantity: newQuantity,
+            csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val(),
+        },
+        success: function (response) {
+            // Maneja la respuesta del servidor si es necesario
+            console.log(response);
+        },
+        error: function (error) {
+            console.error('Error al actualizar la cantidad', error);
         }
-    },
-    invalid: {
-        color: '#dc3545',
-        iconColor: '#dc3545'
-    }
-};
-var card = elements.create('card', {style: style});
-card.mount('#card-element');
+    });
+}
 
-// Handle realtime validation errors on the card element
-card.addEventListener('change', function (event) {
-    var errorDiv = document.getElementById('card-errors');
-    if (event.error) {
-        var html = `
-            <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
-            </span>
-            <span>${event.error.message}</span>
-        `;
-        $(errorDiv).html(html);
-    } else {
-        errorDiv.textContent = '';
+// Ensure proper enabling/disabling of all inputs on page load
+var allQtyInputs = $('.qty_input');
+for (var i = 0; i < allQtyInputs.length; i++) {
+    var itemId = $(allQtyInputs[i]).data('item_id');
+    handleEnableDisable(itemId);
+}
+
+// Check enable/disable every time the input is changed
+$('.qty_input').change(function () {
+    var itemId = $(this).data('item_id');
+    handleEnableDisable(itemId);
+});
+
+// Increment quantity on plus button click
+$(document).on('click', '.increment-qty', function (e) {
+    e.preventDefault();
+    var itemId = $(this).data('item_id');
+    var inputField = $(`.qty_input.id_qty_${itemId}`);
+    var currentValue = parseInt(inputField.val());
+
+    if (currentValue < 99) {
+        var newQuantity = currentValue + 1;
+        inputField.val(newQuantity);
+        handleEnableDisable(itemId);
+        updateQuantity(itemId, newQuantity);
     }
 });
 
-// Handle form submit
-var form = document.getElementById('payment-form');
+// Decrement quantity on minus button click
+$(document).on('click', '.decrement-qty', function (e) {
+    e.preventDefault();
+    var itemId = $(this).data('item_id');
+    var inputField = $(`.qty_input.id_qty_${itemId}`);
+    var currentValue = parseInt(inputField.val());
 
-form.addEventListener('submit', function(ev) {
-    ev.preventDefault();
-    card.update({ 'disabled': true});
-    $('#submit-button').attr('disabled', true);
-    $('#payment-form').fadeToggle(100);
-    $('#loading-overlay').fadeToggle(100);
-
-    var saveInfo = Boolean($('#id-save-info').attr('checked'));
-    // From using {% csrf_token %} in the form
-    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
-    var postData = {
-        'csrfmiddlewaretoken': csrfToken,
-        'client_secret': clientSecret,
-        'save_info': saveInfo,
-    };
-    var url = '/checkout/cache_checkout_data/';
-
-    $.post(url, postData).done(function () {
-        stripe.confirmCardPayment(clientSecret, {
-            payment_method: {
-                card: card,
-                billing_details: {
-                    name: $.trim(form.full_name.value),
-                    phone: $.trim(form.phone_number.value),
-                    email: $.trim(form.email.value),
-                    address:{
-                        line1: $.trim(form.street_address1.value),
-                        line2: $.trim(form.street_address2.value),
-                        city: $.trim(form.town_or_city.value),
-                        country: $.trim(form.country.value),
-                        state: $.trim(form.county.value),
-                    }
-                }
-            },
-            shipping: {
-                name: $.trim(form.full_name.value),
-                phone: $.trim(form.phone_number.value),
-                address: {
-                    line1: $.trim(form.street_address1.value),
-                    line2: $.trim(form.street_address2.value),
-                    city: $.trim(form.town_or_city.value),
-                    country: $.trim(form.country.value),
-                    postal_code: $.trim(form.postcode.value),
-                    state: $.trim(form.county.value),
-                }
-            },
-        }).then(function(result) {
-            if (result.error) {
-                var errorDiv = document.getElementById('card-errors');
-                var html = `
-                    <span class="icon" role="alert">
-                    <i class="fas fa-times"></i>
-                    </span>
-                    <span>${result.error.message}</span>`;
-                $(errorDiv).html(html);
-                $('#payment-form').fadeToggle(100);
-                $('#loading-overlay').fadeToggle(100);
-                card.update({ 'disabled': false});
-                $('#submit-button').attr('disabled', false);
-            } else {
-                if (result.paymentIntent.status === 'succeeded') {
-                    form.submit();
-                }
-            }
-        });
-    }).fail(function () {
-        // just reload the page, the error will be in django messages
-        location.reload();
-    })
+    if (currentValue > 1) {
+        var newQuantity = currentValue - 1;
+        inputField.val(newQuantity);
+        handleEnableDisable(itemId);
+        updateQuantity(itemId, newQuantity);
+    }
 });
